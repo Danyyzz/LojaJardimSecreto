48aa0b34d4835a714a3282fbcf029b3415bbaf2c
Danyyzz <dcelestrineburkowski@gmail.com>
2024-11-23 19:55:27 -0300

    23-11

---
+++ javascript/javascript.js
@@ -0,0 +1,18 @@
+document.addEventListener("scroll", () => {
+    const scrollPosition = window.pageYOffset;
+  
+    const imagem1 = document.getElementById("imagem1");
+    const imagem2 = document.getElementById("imagem2");
+  
+    imagem1.style.transform = `translateY(${scrollPosition * 0.3}px)`;
+    imagem2.style.transform = `translateY(${(scrollPosition - window.innerHeight) * 0.5}px)`;
+  });
+
+
+  document.querySelectorAll('.dropdown-toggle').forEach((dropdown) => {
+    dropdown.addEventListener('click', (e) => {
+      e.preventDefault();
+      const menu = dropdown.nextElementSibling;
+      menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    });
+  });
